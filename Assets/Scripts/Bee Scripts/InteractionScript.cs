7533314d98e4e8a8d32ae9e60c4876fb82d37713
bdevainy <auro.g.r.14@gmail.com>
2022-11-17 02:44:05 +0100

    lots of things
    kinda fixed shooting direction ?
    added fully functional HUD
    bee can pick up honey and has interaction with enemies

--- Assets/Scripts/Bee Scripts/InteractionScript.cs
+++ Assets/Scripts/Bee Scripts/InteractionScript.cs
@@ -7,9 +7,16 @@ public class InteractionScript : MonoBehaviour
 {
     public float interactRadius;
     public bool interacting;
-    public int honeyLevel = 4;
+    public int honeyLevel = 0;
+    public int lives = 4;
     
     public Transform pickPosition;
+
+    public void Awake()
+    {
+        gameObject.GetComponent<BeeUpdateUI>().UpdateHoneySprite(honeyLevel);
+        gameObject.GetComponent<BeeUpdateUI>().UpdateLivesSprite(lives);
+    }
 
     #region Pick Interaction
     public bool picking;
@@ -43,6 +50,7 @@ public class InteractionScript : MonoBehaviour
     {
         if (honeyLevel < 2) return;
         honeyLevel -= 2;
+        gameObject.GetComponent<BeeUpdateUI>().UpdateHoneySprite(honeyLevel);
         interactingObject.GetComponent<Rigidbody>().useGravity = false;
         //interactingObject.transform.parent = pickPosition;
         interactingObject.transform.position = pickPosition.position;
@@ -59,5 +67,39 @@ public class InteractionScript : MonoBehaviour
             pickedItem.transform.position = pickPosition.position;
             pickedItem.transform.rotation = pickPosition.rotation;
         }
+    }
+
+    private void OnCollisionEnter(Collision other)
+    {
+        if (other.gameObject.layer == LayerMask.NameToLayer("Honey"))
+        {
+            if (honeyLevel >= 10)
+            {
+                return;
+            }
+            else
+            {
+                gameObject.GetComponent<BeeUpdateUI>().UpdateHoneySprite(honeyLevel);
+                if (honeyLevel+2 <= 10)
+                {
+                    honeyLevel += 2;
+                }
+                else
+                {
+                    honeyLevel = 10;
+                }
+                
+                Destroy(other.gameObject);
+            }
+
+        }
+        else if (other.gameObject.layer == LayerMask.NameToLayer("Enemy"))
+        {
+            lives--;
+            gameObject.GetComponent<BeeUpdateUI>().UpdateLivesSprite(lives);
+
+            other.gameObject.GetComponent<WanderingAI>().Touched();
+        }
+
     }
 }
